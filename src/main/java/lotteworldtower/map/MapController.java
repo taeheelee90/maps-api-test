17276fec0b3106aa3d485e973f223833b3673bfc
taeheelee90 <taehee.lee90@gmail.com>
2021-02-02 12:48:07 +0200

    Add kakao maps API

--- src/main/java/lotteworldtower/map/MapController.java
+++ src/main/java/lotteworldtower/map/MapController.java
@@ -5,9 +5,19 @@ import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class MapController {
-
+	
 	@GetMapping("/")
+	public String main() {
+		return "index";
+	}
+
+	@GetMapping("/naver")
 	public String naverMap() {
 		return "naver";
+	}
+
+	@GetMapping("/kakao")
+	public String kakaoMap() {
+		return "kakao";
 	}
 }
